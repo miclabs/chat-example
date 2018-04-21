20aac65fd3aa92d1626c65c460cf5496d7d94465
Jacky Vo <jackyinvn@gmail.com>
2018-04-21 14:38:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+io.set('origins', '*:*');
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
